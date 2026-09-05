2938cbce0caf181393f929efcfa6bb07075114ca
etowner <emmanueltowner@gmail.com>
2026-09-05 18:26:11 -0400

    Updated Header

--- frontend/src/components/Home/Header.tsx
+++ frontend/src/components/Home/Header.tsx
@@ -4,14 +4,14 @@ import ThemeToggle from "./ThemeToggle";
 
 const Header = () => (
     <Navbar variant="dark" className="mb-4">
-      <Container>
+     
         <Navbar.Brand>Aegis</Navbar.Brand>
         <Nav className="ms-auto align-items-center">
           <ThemeToggle />
-          <Navbar.Text className="me-2">Signed in as:</Navbar.Text>
+          {/* <Navbar.Text className="me-2">Signed in as:</Navbar.Text> */}
           <ProfileManager />
         </Nav>
-      </Container>
+     
     </Navbar>
 );
 export default Header;
